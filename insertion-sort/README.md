cfaf56972be7e63a7101dca17e032c234ed877b4
Seong Chang <you@example.com>
2018-11-28 23:39:28 -0800

    insertion sort complete

---
+++ insertion-sort/README.md
@@ -0,0 +1,5 @@
+Not much to explain here.\n
+\n
+Worst case: Big O of n^2\n
+Average case: Big O of n^2\n
+Best case: Big O of n\n
---
+++ insertion-sort/insertion_sort.py
@@ -0,0 +1,16 @@
+def main():
+    print('Running insertion sort')
+    array = [5, 2, 4, 6, 1, 3]
+
+    for j in range(1, len(array)):
+        key = array[j]
+        i = j - 1
+        while i >= 0 and array[i] > key:
+            array[i + 1] = array[i]
+            i -= 1
+        array[i + 1] = key
+
+    print(array)
+
+if __name__== "__main__":
+    main()
